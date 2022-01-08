ada73ad055d8dbbb20b08a9634112fa5b83a0ae5
Alexis Evaristo López Gómez <ilegal_sprite@outlook.com>
2022-01-08 11:50:44 -0600

    agregamos hbs

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const express = require( 'express' );
 const app = express();
 const port = 4000;
 
+app.set( 'view engine', 'hbs' );
+
 // * Servir contenido estático
 app.use( express.static( 'public' ) );
 
-// app.get('/', ( req, res ) => {
-//     res.send( 'Home page' );
-// });
+app.get('/', ( req, res ) => {
+    res.render( 'home' );
+});
 
-app.get('/hola-mundo', ( req, res ) => {
-    res.send( 'Home Mundo en su respetiva ruta' );
+app.get('/generic', ( req, res ) => {
+    res.sendFile( __dirname + '/public/generic.html' );
+});
+
+app.get('/elements', ( req, res ) => {
+    res.sendFile( __dirname + '/public/elements.html' );
 });
 
 // * Para que entre si no exite la ruta en la que estan accediendo
 app.get('*', ( req, res ) => {
-    // res.send( '404 | Page not found' );
-    res.sendFile( __dirname + '/public/404.html' );
+    res.send( '404 | Page not found' );
+    // res.sendFile( __dirname + '/public/404.html' );
 });
 
 app.listen( port, () => {
